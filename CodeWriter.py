fb26da340e3cc4c539b5f51262caebf30ab23153
meshi <meshi.djerassi@mail.huji.ac.il>
2019-11-27 14:34:09 +0200

    file handling

--- CodeWriter.py
+++ CodeWriter.py
@@ -3,7 +3,7 @@ BIN_MATH_OPS = {"and": "M=D&M", "or": "M=D|M", "add": "M=D+M", "sub": "D=-D\nM=D
 UNI_MATH_OPS = {"not": "!", "neg": "-"}
 LOGIC_OPS = {"eq": ("FALSE", "FALSE", "EQ"), "lt": ("FALSE", "TRUE", "LT"), "gt": ("TRUE", "FALSE", "GT")}
 BIN_MATH_STR = "D=M\n@SP\nM=M-1\nA=M-1\n"
-UNI_MATH_STR = "M = {}M"
+UNI_MATH_STR = "M = {}M\n"
 LOGIC_STR = "D=M\n@R13//y\nM=D\n@SP\nM=M-1\nA=M-1\nD=M\n@R14//x\nM=D\n\n" \
             "@NEG_X\nD;JLT\n@R13\nD=M\n@{0}\nD;JLT\n@COMPARE\n0;JMP\n\n" \
             "(NEG_X)\n@R13\nD=M\n@{1}\nD;JGT\n@COMPARE\n0;JMP\n\n" \
@@ -14,11 +14,11 @@ HEAP = {"local": "LCL", "argument": "ARG", "this": "THIS", "that": "THAT"}
 CONST_RAM = {"pointer": 3, "temp": 5}
 
 POP_STR_1 = STCK_ACCESS_STR + "D=M\n@R13\nM=D\n@SP\nM=M-1\n"
-POP_STR_2 = "@R14\nM=D\n@R13\nD=M\n@R14\nA=M\nM=D"
+POP_STR_2 = "@R14\nM=D\n@R13\nD=M\n@R14\nA=M\nM=D\n"
 HEAP_CRAM_POP_STR = "@{}\nD={}\n@{}\nD=A+D\n"
-STATIC_POP_STR = "@{}.{}\nD=A"
+STATIC_POP_STR = "@{}.{}\nD=A\n"
 
-PUSH_STR = "D={}\n@SP\nA=M\nM=D\n@SP\nM=M+1"
+PUSH_STR = "D={}\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
 HEAP_CRAM_PUSH_STR = "@{}\nD={}\n@{}\nA=A+D\n"
 STATIC_PUSH_STR = "@{}.{}\n"
 

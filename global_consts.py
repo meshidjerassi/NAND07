f52f8b871efa5f725f1e9139d374dece3d43260a
meshi <meshi.djerassi@mail.huji.ac.il>
2019-11-30 18:59:12 +0200

    bug fix

--- global_consts.py
+++ global_consts.py
@@ -4,4 +4,4 @@ TAB = "\t"
 
 seg = ["local", "argument", "this", "that", "constant", "static", "pointer", "temp"]
 cmd = ["push", "pop"]
-opp = ["add", "neg", "eq", "not", "or", "and", "sub"]
+opp = ["add", "neg", "eq", "not", "or", "and", "sub", "lt", "gt"]
